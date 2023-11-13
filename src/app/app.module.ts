8024e25272659f20bdab2ec7e07b5d933661a75d
AGmaj <adrian.gmaj@gmail.com>
2023-11-13 22:28:41 +0100

    showcase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ShowcaseComponent
+   ],
   imports: [
     BrowserModule
   ],
